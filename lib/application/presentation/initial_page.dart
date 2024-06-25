f13e62a89f5ae9ccb08fae17ff14097b99f19258
IhorDovh <holdargraf@gmail.com>
2024-06-25 09:29:42 +0300

    fix initial_page

--- lib/application/presentation/initial_page.dart
+++ lib/application/presentation/initial_page.dart
@@ -1,6 +1,5 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:proj_base/application/presentation/router/router.gr.dart';
 
 class InitialPage extends StatelessWidget {
@@ -8,12 +7,15 @@ class InitialPage extends StatelessWidget {
 
   @override
   Widget build(final BuildContext context) {
-    final router =
-        AutoRouter.innerRouterOf(context, InitialRoute.name) ?? context.router;
+    WidgetsBinding.instance.addPostFrameCallback((final _) {
+      final router =
+          AutoRouter.innerRouterOf(context, InitialRoute.name) ?? context.router;
 
-    if (router.current.name == MainRoute.name) return const AutoRouter();
-    router.popUntil(ModalRoute.withName('/'));
-    router.replace(MainRoute());
+      if (router.current.name != MainRoute.name) {
+        router.popUntil(ModalRoute.withName('/'));
+        router.replace(MainRoute());
+      }
+    });
 
     return const AutoRouter();
   }
